a2f8ba6b83cb0b3401f837f1403b72a2176a56d1
Brent Johnson <bjohnson@signalpath.com>
2019-02-06 14:41:43 -0500

    Modified proton_c for laplace keyboard. Builds and flashes.

--- keyboards/proton_c/config.h
+++ keyboards/proton_c/config.h
@@ -27,8 +27,8 @@
 #define DESCRIPTION     A compact ortholinear keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 1
-#define MATRIX_COLS 1
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 7
 
 #define MATRIX_ROW_PINS { B9 }
 #define MATRIX_COL_PINS { B0 }
--- keyboards/proton_c/rules.mk
+++ keyboards/proton_c/rules.mk
@@ -1,5 +1,7 @@
 MCU = STM32F303
 
+DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
+
 # Build Options
 #   comment out to disable the options.
 #
